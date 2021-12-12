34700b122897e5c99f05e600195491d5cf1367d4
Zsolt Gomori <zsgomoridev@icloud.com>
2021-12-12 15:01:30 +0100

    install and configure tailwindcss typography plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,43 @@ module.exports = {
       fontFamily: {
         sans: ['Montserrat', ...fontFamily.sans],
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            h1: {
+              color: theme('colors.neutral.200'),
+              fontWeight: 700,
+            },
+            h2: {
+              color: theme('colors.neutral.200'),
+              fontWeight: 500,
+            },
+            p: {
+              color: theme('colors.neutral.400'),
+            },
+            'ul > li, ol > li': {
+              color: theme('colors.neutral.400'),
+            },
+            'ul > li::marker, ol > li::marker': {
+              color: theme('colors.neutral.200'),
+            },
+            strong: {
+              color: theme('colors.neutral.200'),
+            },
+            em: {
+              color: theme('colors.neutral.200'),
+            },
+            a: {
+              color: theme('colors.sky.500'),
+              textDecorationColor: theme('colors.sky.500'),
+            },
+          },
+        },
+      }),
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
