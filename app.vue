789028c797303279c64a03af3fb4c3743132a803
gomorizsolt <zsgomoridev@icloud.com>
2024-10-23 13:52:14 +0200

    feat(layout): add desktop header

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <Body class="bg-neutral-900">
     <div class="flex min-h-screen flex-col">
+      <Header />
+
       <main class="mx-auto flex w-full max-w-3xl flex-1 flex-col px-6 py-14 md:px-0">
         <NuxtPage />
       </main>
---
+++ components/Header.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+const NAV_ITEMS = [
+  { label: "home", to: "/" },
+  { label: "about", to: "/about" },
+  { label: "blog", to: "/blog" },
+  { label: "projects", to: "/projects" },
+  { label: "uses", to: "/uses" },
+];
+</script>
+
+<template>
+  <header class="flex items-center justify-between p-8 text-sm">
+    <NuxtLink to="/">
+      <NuxtImg
+        src="/avatar.jpeg"
+        format="webp"
+        loading="lazy"
+        alt="Picture of me while hiking"
+        class="h-12 w-12 rounded-full border-2 border-solid border-neutral-100"
+        placeholder
+      />
+    </NuxtLink>
+
+    <nav class="max-w-3xl flex-1">
+      <ul class="flex items-center justify-between">
+        <li v-for="{ label, to } in NAV_ITEMS" :key="label">
+          <NuxtLink class="rounded-lg py-2 px-3 font-medium transition-colors duration-100 ease-in text-neutral-300 hover:bg-neutral-800 hover:text-neutral-100" :to="to">
+            {{ label }}
+          </NuxtLink>
+        </li>
+      </ul>
+    </nav>
+
+    <div class="p-1.5" />
+  </header>
+</template>
