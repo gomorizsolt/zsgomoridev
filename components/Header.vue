c422c53a02187678de4071ee292d921e1bd8b756
gomorizsolt <zsgomoridev@icloud.com>
2024-10-23 14:16:16 +0200

    feat(header): implement mobile menu

--- components/Header.vue
+++ components/Header.vue
@@ -6,23 +6,47 @@ const NAV_ITEMS = [
   { label: "projects", to: "/projects" },
   { label: "uses", to: "/uses" },
 ];
+
+const isOpen = ref(false);
 </script>
 
 <template>
   <header class="flex items-center justify-between p-8 text-sm">
+    <button
+      :class="['tham tham-e-spin tham-w-6 z-20 md:hidden', isOpen && 'tham-active']"
+      aria-label="Toggle menu"
+      @click="isOpen = !isOpen"
+    >
+      <div class="tham-box">
+        <div class="tham-inner bg-neutral-300" />
+      </div>
+    </button>
+
+    <div v-show="isOpen" class="absolute top-0 left-0 z-10 h-full w-full bg-neutral-900">
+      <nav class="grid h-full w-full content-center justify-center">
+        <ul class="flex flex-col gap-y-4 transition-opacity duration-200">
+          <li v-for="{ label, to } in NAV_ITEMS" :key="label">
+            <NuxtLink class="text-2xl font-medium text-neutral-300 transition-colors duration-150 ease-in hover:text-neutral-100" :to="to">
+              {{ label }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+
     <NuxtLink to="/">
       <NuxtImg
         src="/avatar.jpeg"
         format="webp"
         loading="lazy"
         alt="Picture of me while hiking"
-        class="h-12 w-12 rounded-full border-2 border-solid border-neutral-100"
+        class="h-10 w-10 rounded-full border-2 border-solid border-neutral-100 md:h-12 md:w-12"
         placeholder
       />
     </NuxtLink>
 
-    <nav class="max-w-3xl flex-1">
-      <ul class="flex items-center justify-between">
+    <nav class="max-w-3xl flex-1 hidden md:block">
+      <ul class="flex items-center justify-end gap-x-4 lg:gap-x-8">
         <li v-for="{ label, to } in NAV_ITEMS" :key="label">
           <NuxtLink class="rounded-lg py-2 px-3 font-medium transition-colors duration-100 ease-in text-neutral-300 hover:bg-neutral-800 hover:text-neutral-100" :to="to">
             {{ label }}
@@ -30,7 +54,5 @@ const NAV_ITEMS = [
         </li>
       </ul>
     </nav>
-
-    <div class="p-1.5" />
   </header>
 </template>
