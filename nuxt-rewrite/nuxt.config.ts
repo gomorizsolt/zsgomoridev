575aa427f008408d6270040f9c38b8153ecc9f44
gomorizsolt <zsgomoridev@icloud.com>
2024-10-23 09:44:46 +0200

    build: enable eslint stylistic

--- nuxt-rewrite/nuxt.config.ts
+++ nuxt-rewrite/nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  
+  modules: ['@nuxt/eslint'],
+
+  eslint: {
+    config: {
+      stylistic: {
+        semi: true,
+        quotes: "double",
+        arrowParens: true,
+      },
+    },
+  },
 })
