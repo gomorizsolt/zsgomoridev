26a29b7f2edcf4195a5b52e84277a90a44b1afe6
gomorizsolt <zsgomoridev@icloud.com>
2024-10-24 10:30:23 +0200

    feat(footer): add LINKS const

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,30 +1,36 @@
+<script setup lang="ts">
+const LINKS = [
+  {
+    label: "GitHub",
+    link: "https://github.com/gomorizsolt",
+    icon: "i-fa6-brands-square-github",
+  },
+
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/zsgomori/",
+    icon: "i-fa6-brands-linkedin",
+  },
+
+  {
+    label: "Twitter/X",
+    link: "https://twitter.com/zsgomori",
+    icon: "i-fa6-brands-square-x-twitter",
+  },
+];
+</script>
+
 <template>
   <footer class="flex items-center gap-x-6 py-6 pl-8">
     <NuxtLink
-      aria-label="GitHub"
+      v-for="{ icon, label, link } in LINKS"
+      :key="label"
+      :href="link"
+      :aria-label="label"
       target="_blank"
-      href="https://github.com/gomorizsolt"
       class="text-neutral-300 transition-colors duration-150 hover:text-neutral-100"
     >
-      <span aria-hidden class="i-fa6-brands-square-github h-5 w-5 md:h-6 md:w-6" />
-    </NuxtLink>
-
-    <NuxtLink
-      aria-label="LinkedIn"
-      target="_blank"
-      href="https://www.linkedin.com/in/zsgomori/"
-      class="text-neutral-300 transition-colors duration-150 hover:text-neutral-100"
-    >
-      <span aria-hidden class="i-fa6-brands-linkedin h-5 w-5 md:h-6 md:w-6" />
-    </NuxtLink>
-
-    <NuxtLink
-      aria-label="Twitter"
-      target="_blank"
-      href="https://twitter.com/zsgomori"
-      class="text-neutral-300 transition-colors duration-150 hover:text-neutral-100"
-    >
-      <span aria-hidden class="i-fa6-brands-square-x-twitter h-5 w-5 md:h-6 md:w-6" />
+      <span aria-hidden :class="[icon, 'h-5 w-5 md:h-6 md:w-6']" />
     </NuxtLink>
   </footer>
 </template>
