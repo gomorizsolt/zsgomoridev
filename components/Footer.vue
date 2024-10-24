3072f2b1d53ebcee9d0bcf8a3b653ad7b65a1b8f
gomorizsolt <zsgomoridev@icloud.com>
2024-10-24 14:58:59 +0200

    feat(footer): render tooltips

--- components/Footer.vue
+++ components/Footer.vue
@@ -22,15 +22,21 @@ const LINKS = [
 
 <template>
   <footer class="flex items-center gap-x-6 py-6 pl-8">
-    <NuxtLink
-      v-for="{ icon, label, link } in LINKS"
-      :key="label"
-      :href="link"
-      :aria-label="label"
-      target="_blank"
-      class="text-neutral-300 transition-colors duration-150 hover:text-neutral-100"
-    >
-      <span aria-hidden :class="[icon, 'h-5 w-5 md:h-6 md:w-6']" />
-    </NuxtLink>
+    <UITooltip v-for="{ icon, label, link } in LINKS" :key="label">
+      <UITooltipTrigger as-child>
+        <NuxtLink
+          :href="link"
+          :aria-label="label"
+          target="_blank"
+          class="text-neutral-300 transition-colors duration-150 hover:text-neutral-100"
+        >
+          <span aria-hidden :class="[icon, 'h-5 w-5 md:h-6 md:w-6']" />
+        </NuxtLink>
+      </UITooltipTrigger>
+
+      <UITooltipContent>
+        {{ label }}
+      </UITooltipContent>
+    </UITooltip>
   </footer>
 </template>
