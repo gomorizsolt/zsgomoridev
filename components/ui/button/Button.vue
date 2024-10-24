f9d8e61d6130291b83b7d3609aa6515567569051
gomorizsolt <zsgomoridev@icloud.com>
2024-10-24 14:23:35 +0200

    feat(components): add Button

---
+++ components/ui/button/Button.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue";
+import { Primitive, type PrimitiveProps } from "radix-vue";
+import { type ButtonVariants, buttonVariants } from ".";
+
+interface ButtonProps extends PrimitiveProps {
+  variant?: ButtonVariants["variant"];
+  size?: ButtonVariants["size"];
+  class?: HTMLAttributes["class"];
+}
+
+const { as = "button", class: _class = "", variant, size } = defineProps<ButtonProps>();
+
+const mergedClasses = computed(() => mergeClasses(buttonVariants({
+  variant,
+  size,
+}), _class));
+</script>
+
+<template>
+  <Primitive :as="as" :as-child="asChild" :class="mergedClasses">
+    <slot />
+  </Primitive>
+</template>
