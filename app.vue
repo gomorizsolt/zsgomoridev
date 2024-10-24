18a83ab6ec255d7871e1cf9152ecdac3c523c217
gomorizsolt <zsgomoridev@icloud.com>
2024-10-24 14:58:45 +0200

    feat: wrap app with UITooltipProvider

--- app.vue
+++ app.vue
@@ -7,14 +7,16 @@ useSeoMeta({
 
 <template>
   <Body class="bg-neutral-900">
-    <div class="flex min-h-screen flex-col">
-      <Header />
+    <UITooltipProvider :delay-duration="400">
+      <div class="flex min-h-screen flex-col">
+        <Header />
 
-      <main class="mx-auto flex w-full max-w-3xl flex-1 flex-col px-6 py-14 md:px-0">
-        <NuxtPage />
-      </main>
+        <main class="mx-auto flex w-full max-w-3xl flex-1 flex-col px-6 py-14 md:px-0">
+          <NuxtPage />
+        </main>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </UITooltipProvider>
   </Body>
 </template>
