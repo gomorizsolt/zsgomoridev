743d5623551ec86473d46068c3803b89e866f93e
gomorizsolt <zsgomoridev@icloud.com>
2024-10-25 15:13:05 +0200

    style(container): adjust padding

--- components/global/container.vue
+++ components/global/container.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mx-auto flex w-full max-w-3xl flex-col px-6 py-14 md:px-0">
+  <div class="mx-auto flex w-full max-w-3xl flex-col px-6 py-14 lg:px-0">
     <slot />
   </div>
 </template>
