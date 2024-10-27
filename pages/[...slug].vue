05bf798f07db9430d51e470e49487f5efbe7dd78
gomorizsolt <zsgomoridev@icloud.com>
2024-10-27 10:02:29 +0100

    style: adjust typography styles on mobiles

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,3 +1,3 @@
 <template>
-  <ContentDoc class="max-w-none prose prose-neutral prose-invert prose-headings:text-neutral-200 prose-p:text-justify" />
+  <ContentDoc class="max-w-none prose prose-sm md:prose-base prose-neutral prose-invert prose-headings:text-neutral-200 prose-p:text-justify" />
 </template>
